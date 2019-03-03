053be16c9ec9b25ba3bcddbbfc9ac1e942c843e9
FaBii <FaBii@DESKTOP-KKN58F9>
2019-03-03 15:46:41 -0600

    avance

--- AppEcomonedas/AppEcomonedas/s.aspx.cs
+++ AppEcomonedas/AppEcomonedas/s.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace AppEcomonedas
 {
-    public partial class Inicio1 : System.Web.UI.Page
+    public partial class Inicio : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
