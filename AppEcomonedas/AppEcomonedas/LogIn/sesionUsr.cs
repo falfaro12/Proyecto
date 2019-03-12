a789de861aa520c0f2468d2005f01f650f9a3d9e
Ramon Rodriguez <ramonrdd3@gmail.com>
2019-03-11 20:44:42 -0600

    Re instale el entityframework. Cree una clase llamada sesionUsr que servira para la sesion. Inicie la clase de UsuarioLN

---
+++ AppEcomonedas/AppEcomonedas/LogIn/sesionUsr.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Contexto;
+
+namespace AppEcomonedas.LogIn
+{
+    public class SesionUsr
+    {
+
+        //Implementación Singleton
+
+        // Las propiedades de solo lectura solo se pueden establecer en la inicialización o en un constructor
+        public static readonly Usuario Instancia;
+
+        // Se llama al constructor estático tan pronto como la clase se carga en la memoria
+        static SesionUsr()
+        {
+            // Si el carrito no está en la sesión, cree uno y guarde los items.
+            if (HttpContext.Current.Session["usuario"] == null)
+            {
+                Instancia = new Usuario();
+                HttpContext.Current.Session["usuario"] = Instancia;
+            }
+            else
+            {
+                // De lo contrario, obténgalo de la sesión.
+                Instancia = (Usuario)HttpContext.Current.Session["usuario"];
+            }
+        }
+
+        // Un constructor protegido asegura que un objeto no se puede crear desde el exterior
+        protected SesionUsr() { }
+
+        public void iniciarSesion(int id, string password)
+        {
+
+        }
+
+    }
+}
---
+++ AppEcomonedas/Contexto/LN/UsuarioLN.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contexto.LN
+{
+    public class UsuarioLN
+    {
+        public static bool AgregarUsuario(
+            string nombre,
+            int idRol,
+            int telefono,
+            string idUsr = ""
+            )
+        {
+            EcomonedasContexto db = new EcomonedasContexto();
+            var miUsuario = new Usuario();
+            int idUser = 0;
+            string idUsuario = "";
+            bool esNumero = int.TryParse(idUsr, out idUser);
+            if (esNumero)
+                idUsuario = idUser.ToString();
+
+            if (esNumero || idUser> 0)
+            {
+                // Busca Usuario en la DB
+                miUsuario = db.Usuario.Where(p => p.Id_Usuario == idUsuario).First<Usuario>();
+            }
+            //Creacion del libro
+            miUsuario.Nombre = nombre;
+            miUsuario.Id_Usuario = idUsr;
+            miUsuario.Id_Rol = idRol;
+
+            if (idUsr.Equals("") || !esNumero)
+            { //agrega
+                db.Usuario.Add(miUsuario);
+            }
+            db.SaveChanges();
+
+            // Confirmacion
+            return true;
+        }
+
+
+
+        public static IQueryable queryListaUsuario()
+        {
+            var db = new EcomonedasContexto();
+            IQueryable query = db.Usuario;
+            return query;
+        }
+
+        public static IEnumerable<Usuario> listaUsuarios()
+        {
+            IEnumerable<Usuario> lista = (IEnumerable<Usuario>)
+                UsuarioLN.queryListaUsuario();
+            return lista;
+        }
+        public static Usuario obtenerUsuario(string id)
+        {
+            EcomonedasContexto db = new EcomonedasContexto();
+            Usuario Usuario = db.Usuario.
+                    Where(p => p.Id_Usuario == id).
+                    First<Usuario>();
+            return Usuario;
+        }
+    }
+}
