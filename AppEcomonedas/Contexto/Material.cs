69869ca711282e5332e74254082a5483c467da26
FaBii <FaBii@DESKTOP-KKN58F9>
2019-03-24 13:32:16 -0600

    Revert "bdj"
    This reverts commit ed737326ca8c4a6a674f19f36db197e590671eec.

--- AppEcomonedas/Contexto/Material.cs
+++ AppEcomonedas/Contexto/Material.cs
@@ -28,7 +28,7 @@ namespace Contexto
 
         public string imagen { get; set; }
 
-        [StringLength(50)]
+        [StringLength(10)]
         public string color { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
